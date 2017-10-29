e265ac50719358c3064f205a1b835f985ef95465
Marco Kisperth <kisperth@me.com>
2017-10-29 14:15:21 +0100

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Marco Kisperth on 29.10.17.
+//  Copyright © 2017 Marco Kisperth. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary : NSObject {
+  
+}
